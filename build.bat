ea24ad5bced00d9572c290884e56cab7ae3c48ae
Finn Hartshorn <finnhartshorn@gmail.com>
2023-07-30 13:25:44 +1000

    Added batch file to build with defaults and copy to zoo tycoon directory for non-injected run

---
+++ build.bat
@@ -0,0 +1,19 @@
+@echo off
+
+REM Run cargo build with specified arguments
+cargo +nightly build --lib --release --target=i686-pc-windows-msvc %*
+
+REM Check if the build succeeded
+if %ERRORLEVEL% neq 0 (
+    echo Cargo build failed.
+    pause
+    exit /b
+)
+
+REM Copy the file to the destination
+copy "target\i686-pc-windows-msvc\release\openzt.dll" "C:\Program Files (x86)\Microsoft Games\Zoo Tycoon\lang003-openzt.dll"
+
+REM Run the zoo.exe executable
+start "Zoo Tycoon" "C:\Program Files (x86)\Microsoft Games\Zoo Tycoon\zoo.exe"
+
+exit /b
