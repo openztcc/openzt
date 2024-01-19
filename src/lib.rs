86ac9b746e31bdad1135522cdf66dd9781520973
Finn Hartshorn <finnhartshorn@gmail.com>
2024-01-19 12:01:51 +1100

    Get OpenZT version from cargo env var

--- src/lib.rs
+++ src/lib.rs
@@ -114,15 +114,14 @@ mod bf_version_info {
 
     use tracing::info;
 
-    const OPENZT_VERSION_STR: &str = " OpenZT: 0.1.0";
-
     #[hook(unsafe extern "cdecl" BFVersionInfo_GetVersionStringHook, offset = 0x000bdfd4)]
     fn bf_version_info_get_version_string_hook(param_1: u32, param_2: u32, param_3: u32) -> u32 {
         let return_value = unsafe { BFVersionInfo_GetVersionStringHook.call(param_1, param_2, param_3) };
         let version_string = get_string_from_memory(get_from_memory::<u32>(param_2));
         let version_length = version_string.len();
-        save_string_to_memory(get_from_memory::<u32>(param_2) + version_length as u32, OPENZT_VERSION_STR);
-        save_to_memory(param_3, (version_length + OPENZT_VERSION_STR.len() + 2) as u32);
+        let full_openzt_version_string = format!(" OpenZT: {}", env!("CARGO_PKG_VERSION"));
+        save_string_to_memory(get_from_memory::<u32>(param_2) + version_length as u32, &full_openzt_version_string);
+        save_to_memory(param_3, (version_length + full_openzt_version_string.len() + 2) as u32);
         return_value
     }
 }
