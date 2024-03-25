5e70cf2f335ed5f0d84882ab9105fa0096f653ab
Eric Galvan <35750901+GoosiferIO@users.noreply.github.com>
2024-03-25 14:32:30 -0700

    EntityTypes Hooks Implementation (#5)
    * defined and implemented entitytype api
    
    * fixed all getters, now retrieving correct values. added 'print_selected_configuration'
    
    * entitytypes: subarguments for every config option
    
    * comments to unimplemented getters, pointer changes
    
    * initial refactoring of BFEntityType struct and hooks
    
    * removed string implementations for now, replaced with getters
    
    * fixed final errors with memory alignment, correct printed pointer to type
    
    ---------
    
    Co-authored-by: Finn Hartshorn <finnhartshorn@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,10 @@ mod ztadvterrainmgr;
 
 mod expansions;
 
+mod bfentitytype;
+
 mod ztgamemgr;
+
 
 #[cfg(target_os = "windows")]
 use winapi::um::winnt::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH, DLL_THREAD_ATTACH, DLL_THREAD_DETACH};
@@ -201,6 +204,7 @@ extern "system" fn DllMain(module: u8, reason: u32, _reserved: u8) -> i32 {
                 ztworldmgr::init();
                 resource_manager::init();
                 expansions::init();
+                bfentitytype::init();
                 ztgamemgr::init();
             }
 
