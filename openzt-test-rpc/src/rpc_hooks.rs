34d208d6f0cda985fb5bbe266894fb5cb6e140e9
Finn Hartshorn <finnhartshorn@gmail.com>
2025-07-23 21:32:46 +1000

    Initial work on unit testing macro

--- openzt-test-rpc/src/rpc_hooks.rs
+++ openzt-test-rpc/src/rpc_hooks.rs
@@ -15,6 +15,15 @@ macro_rules! generate_allocate_deallocate_named {
                     let _ = Box::from_raw(ptr as *mut $type);
                 }
             }
+
+            #[fmt_function]
+            pub fn [<show_ $name>](ptr: u32) -> u32 {
+                unsafe {
+                    let item = Box::from_raw(ptr as *mut $type);
+                    info!("Received {}: {:?}", stringify!($name), item);
+                    [<show_ $name>](item)
+                }
+            }
         }
     };
 }
@@ -38,13 +47,13 @@ pub fn show_int(num: i32) {
     info!("Received number: {}", num);
 }
 
-#[fmt_function]
-pub fn show_ivec3(vec_ptr: u32) {
-    unsafe {
-        let vec = Box::from_raw(vec_ptr as *mut openztlib::ztworldmgr::IVec3);
-        info!("Received IVec3: ({}, {}, {})", vec.x, vec.y, vec.z);
-    }
-}
+// #[fmt_function]
+// pub fn show_ivec3(vec_ptr: u32) {
+//     unsafe {
+//         let vec = Box::from_raw(vec_ptr as *mut openztlib::ztworldmgr::IVec3);
+//         info!("Received IVec3: ({}, {}, {})", vec.x, vec.y, vec.z);
+//     }
+// }
 
 // #[fmt_function]
 // pub fn allocate_bftile(tile: openzt::ztmapview::BFTile) -> u32 {
